e7e7c09a85d83a4f63e4bb8407eefab632b3044a
Chris McGuire <cdmcguire000@gmail.com>
2018-10-26 11:19:27 -0700

    added the butler font

--- wordpress-4.9.8/wordpress/wp-content/themes/haflife_theme/functions.php
+++ wordpress-4.9.8/wordpress/wp-content/themes/haflife_theme/functions.php
@@ -131,7 +131,9 @@ function haflife_theme_scripts() {
 	// Font Aweseome
 	// wp_enqueue_style('font-awesome', 'https://use.fontawesome.com/releases/v5.0.13/css/all.css');
 	wp_enqueue_style('font-awesome', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css', array(), $ver = null);
+	//Google Font
 
+	wp_enqueue_style('Libre Baskerville', 'href="https://fonts.googleapis.com/css?family=Libre+Baskerville');
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
