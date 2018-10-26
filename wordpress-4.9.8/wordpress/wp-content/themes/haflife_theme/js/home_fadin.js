a313551e592ea942f36b47e3e74451f6f0958322
Chris McGuire <cdmcguire000@gmail.com>
2018-10-25 19:53:03 -0700

    Rough draft of homepage is done

--- wordpress-4.9.8/wordpress/wp-content/themes/haflife_theme/js/home_fadin.js
+++ wordpress-4.9.8/wordpress/wp-content/themes/haflife_theme/js/home_fadin.js
@@ -4,9 +4,32 @@
  * Handles toggling the navigation menu for small screens and enables TAB key
  * navigation support for dropdown menus.
  */
-var missionStatement = jQuery('mission-statement');
-console.log('11');
-jQuery(window).scroll( function(){
-  console.log('1');
-  alert('scroll');
+
+jQuery(document).ready( function(){
+    // turn the right border of the nav menu gold when the user hovers over a link
+    // jQuery('#masthead').on('hover', '.main-navigation li', function() {
+    //     jQuery('.main-navigation ul').css({borderRight: '1px solid #ffd700'});
+    //     console.log('1');
+    // });
+    // jQuery('.main-navigation li').hover(function() {
+    //     jQuery('.main-navigation ul').css({borderRight: '1px solid #ffd700'});
+    // }, function() {
+    //     jQuery('.main-navigation ul').css({borderRight: '1px solid #fff'});
+    // });
+    //
+    // jQuery('main-navigation a').click( function(){
+    //     jQuery('.main-navigation ul').css({borderRight: '1px solid #ffd700'});
+    //     console.log('mousedown');
+    // });
+
+    //jQuery('.background-logo').animate({opacity: '.1'}, [1000000]);
+    jQuery('#content').click( function() {
+        console.log(this);
+        console.log('click');
+        jQuery('.home-tagline').fadeTo(800, 1);
+        jQuery('.mission-statement').fadeTo(800, 1);
+        jQuery('.home-welcome').fadeTo(800, 1);
+        jQuery('#gold-wrapper').fadeTo(800, .3);
+        jQuery('.background-logo').fadeTo(800, .1);
+    })
 });
