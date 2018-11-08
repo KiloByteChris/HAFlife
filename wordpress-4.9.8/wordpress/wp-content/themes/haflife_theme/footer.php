8c0eae43086df021f0b33039508ca8bd05058bd9
Chris McGuire <cdmcguire000@gmail.com>
2018-11-07 19:50:08 -0800

    created site footer

--- wordpress-4.9.8/wordpress/wp-content/themes/haflife_theme/footer.php
+++ wordpress-4.9.8/wordpress/wp-content/themes/haflife_theme/footer.php
@@ -15,17 +15,8 @@
 
 	<footer id="colophon" class="site-footer">
 		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'haflife_theme' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'haflife_theme' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'haflife_theme' ), 'haflife_theme', '<a href="http://underscores.me/">Underscores.me</a>' );
-				?>
+			<p>&copy;2019 HAFLife Entertainment</p>
+			<p>Site created by: <a href="http://www.newvolumewebdev.com/">New Volume Web Development</a></p>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
