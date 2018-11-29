f37ab0c00360d99d31d29150dae9316065e985fb
Chris McGuire <cdmcguire000@gmail.com>
2018-11-28 18:31:41 -0800

    Update home_fadin.js

--- wordpress-4.9.8/wordpress/wp-content/themes/haflife_theme/js/home_fadin.js
+++ wordpress-4.9.8/wordpress/wp-content/themes/haflife_theme/js/home_fadin.js
@@ -7,26 +7,29 @@
 
 jQuery(document).ready( function(){
     var scrollStart = jQuery(window).scrollTop();
+    var scrollCount = 0;
     var backgroundLogo = jQuery('.background-logo');
     var goldWrapper = jQuery('#gold-wrapper');
-    backgroundLogo.fadeTo(600, 1);
-    // if(scrollStart <= 49) {
-    //     backgroundLogo.delay(800).css({opacity: '1'});
-    //     goldWrapper.css({opacity: '0'});
-    // }
-    // if(scrollStart > 49){
-    //     backgroundLogo.css({opacity: '.1'});
-    //     goldWrapper.css({opacity: '.3'});
-    // }
+    // backgroundLogo.fadeTo(600, 1);
+    if(scrollStart <= 150) {
+        backgroundLogo.delay(800).css({opacity: '1'});
+        goldWrapper.css({opacity: '0'});
+    }
+    if(scrollStart > 150){
+        backgroundLogo.css({opacity: '.1'});
+        goldWrapper.css({opacity: '.3'});
+    }
     jQuery( window ).scroll(function() {
         var scrollPosition = jQuery(window).scrollTop();
-        // if(scrollPosition <= 49 ) {
-        //     backgroundLogo.fadeTo(800, 1);
-        //     goldWrapper.fadeTo(800, 0);
-        // }
-        if(scrollPosition > 49 ) {
-            backgroundLogo.fadeTo(800, .1);
-            goldWrapper.fadeTo(800, .3);
+        var logoOpacity = backgroundLogo.css('opacity');
+        if(scrollPosition <=150 && logoOpacity <= .5){
+            console.log(logoOpacity);
+            backgroundLogo.css({opacity: '1'});
+            goldWrapper.css({opacity: '0'});
+        }else if(scrollPosition >150 && logoOpacity > .5){
+            backgroundLogo.css({opacity: '.1'});
+            goldWrapper.css({opacity: '.3'});
+
         }
     });
 });
